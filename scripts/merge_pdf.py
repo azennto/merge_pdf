4bc4487315f031398806afe36e6ab03cf22f3fbd
kubo.y <t320033@m.chukyo-u.ac.jp>
2023-05-02 14:29:15 +0900

    奇数ページの時に空白のページを追加する

--- scripts/merge_pdf.py
+++ scripts/merge_pdf.py
@@ -1,15 +1,20 @@
 import pypdf
 from pathlib import Path
 import sys
+import os
 
 if __name__ == "__main__":
 	pdf_dir_path = Path(sys.argv[1])
 	dir_name = pdf_dir_path.name
 	if pdf_dir_path.is_dir() :
-		merger = pypdf.PdfMerger()
+		dst = pypdf.PdfWriter()
 		for pdf_file in pdf_dir_path.glob("**/*.pdf"):
-			merger.append(pdf_file)
-		merger.write(f"./{dir_name}_merged.pdf")
-		merger.close()
+			reader = pypdf.PdfReader(pdf_file)
+			for page in reader.pages:
+				dst.add_page(page)
+			if len(reader.pages) % 2 == 1:
+				dst.add_blank_page()
+		dst.write(f"{os.getcwd()}/{dir_name}_merged.pdf")
+		dst.close()
 	else:
 		raise ValueError("The argument must be a directory path.")
